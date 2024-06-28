9a0c614bbf3f756f099197626ad2ecfe61c5c7b5
Karthik Reddy Kasarla <126992796+KarthikReddyy22@users.noreply.github.com>
2024-06-28 11:22:32 -0400

    Add files via upload

---
+++ comingsoon.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+def comingsoon(url):
+    res = requests.get(url)
+    res.raise_for_status()
+    soup = BeautifulSoup(res.text, "html.parser")
+    lm = soup.find('div', id="coming-soon-carousel")
+    coming = lm.select('h3')
+    print("\nComing soon movies...""\n" )
+    for i in coming:
+        print(i.get_text())
+        print("\n")
+
+
+
+comingsoon('https://www.fandango.com/')
